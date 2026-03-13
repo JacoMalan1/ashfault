0e7e033b75b51d22d14c405f29c6bd4a36a6ad74
Jaco Malan <jacomalan.12@gmail.com>
2026-03-13 15:31:45 +0200

    Fix layer stack m_InsertPosition iterator

--- src/core/layer_stack.cpp
+++ src/core/layer_stack.cpp
@@ -2,26 +2,41 @@
 #include <ashfault/core/layer_stack.h>
 
 namespace ashfault {
-LayerStack::LayerStack() : m_Layers(), m_InsertPosition(m_Layers.begin()) {}
+LayerStack::LayerStack() : m_Layers(), m_InsertPosition(0) {}
 LayerStack::~LayerStack() {
   for (auto *layer : m_Layers) {
+    layer->on_detach();
     delete layer;
   }
 }
 
 void LayerStack::push_layer(Layer *layer) {
-  this->m_Layers.insert(m_InsertPosition++, layer);
+  this->m_Layers.insert(m_Layers.begin() + m_InsertPosition, layer);
+  m_InsertPosition++;
+  layer->on_attach();
 }
 
 void LayerStack::push_overlay(Layer *layer) {
   this->m_Layers.push_back(layer);
+  layer->on_attach();
 }
 
 void LayerStack::pop_layer(Layer *layer) {
-  for (auto it = m_Layers.begin(); it != m_Layers.end(); it++) {
+  for (auto it = m_Layers.begin(); it != m_Layers.begin() + m_InsertPosition; it++) {
     if (*it == layer) {
+      layer->on_detach();
       m_Layers.erase(it);
+      m_InsertPosition--;
       return;
+    }
+  }
+}
+
+void LayerStack::pop_overlay(Layer *layer) {
+  for (auto it = m_Layers.begin() + m_InsertPosition; it != m_Layers.end(); it++) {
+    if (*it == layer) {
+      layer->on_detach();
+      m_Layers.erase(it);
     }
   }
 }
